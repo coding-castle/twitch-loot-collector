bf823aabf3f6307b5043798b5413979d4728c73e
Patrick <patrickvongoeler@gmail.com>
2021-05-24 16:08:34 +0200

    fix undefined

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 const querySelector = 'button[aria-label="Claim Bonus"]'
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
   setInterval(() => {
     console.log(document.URL)
   }, 5000)
