5c62a1be093c5bb8b0f570c98fa59e4fbd5bb0d0
Patrick <patrickvongoeler@gmail.com>
2021-05-24 18:25:03 +0200

    test

--- LootCollector.js
+++ LootCollector.js
@@ -2,13 +2,13 @@ console.log("Loot Collector init");
 const querySelector = 'button[aria-label="Claim Bonus"]';
 
 setInterval(() => {
-  console.log("interval run", document.URL);
-  const loot = document.querySelector(querySelector);
-  if (loot) {
-    loot.click();
-  }
+  chrome.storage.sync.get("isEnabled", (data) => {
+    const { isEnabled } = data;
+    if (isEnabled) {
+      const loot = document.querySelector(querySelector);
+      if (loot) {
+        loot.click();
+      }
+    }
+  });
 }, 5000);
-
-chrome.storage.sync.get("isEnabled", (data) => {
-  console.log("storage data", data);
-});
