0cadaa8c8b09ecb7f6876b8f6089e149ef233e78
Patrick <patrickvongoeler@gmail.com>
2021-05-24 16:07:09 +0200

    test background

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
-let color = '#3aa757';
+const querySelector = 'button[aria-label="Claim Bonus"]'
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
+  setInterval(() => {
+    console.log(document.URL)
+  }, 5000)
 });
