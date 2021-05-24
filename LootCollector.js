e26fac519d20a9c0c177c0a29180034840047398
Patrick <patrickvongoeler@gmail.com>
2021-05-24 16:44:45 +0200

    test

--- LootCollector.js
+++ LootCollector.js
@@ -1,5 +1,10 @@
-console.log("Loot Collector run")
+console.log("Loot Collector init")
+const querySelector = 'button[aria-label="Claim Bonus"]'
 
 setInterval(() => {
     console.log("interval run", document.URL)
+    const loot = document.querySelector(querySelector)
+    if (loot) {
+        loot.click()
+    }
 }, 5000)
