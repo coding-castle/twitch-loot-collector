5e716282a499d4be48f1f328c9b4fba5574f94c9
Patrick <patrickvongoeler@gmail.com>
2021-05-24 16:26:19 +0200

    test

---
+++ LootCollector.js
@@ -0,0 +1,5 @@
+console.log("Loot Collector run")
+
+setInterval(() => {
+    console.log("interval run", document.URL)
+}, 5000)
--- popup.js
+++ popup.js
@@ -1,8 +1,6 @@
 // Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
 
-let interval = null
-
 // chrome.storage.sync.get("color", ({ color }) => {
 //   changeColor.style.backgroundColor = color;
 // });
@@ -10,20 +8,12 @@ let interval = null
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
   console.log("clicked")
-  if (interval !== null) {
-    clearInterval(interval)
-    interval = null
-  } else {
-    interval = setInterval(async () => {
-
-      let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: claimBonusLoot,
-      });
-    }, 5000)
-  }
+    // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    // chrome.scripting.executeScript({
+    //   target: { tabId: tab.id },
+    //   function: claimBonusLoot,
+    // });
 });
 
 // The body of this function will be execuetd as a content script inside the
